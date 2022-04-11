34d0e990ca88066ac3a0a9960b56971ff8ea8631
ist196858 <eduardo.pamplona@tecnico.ulisboa.pt>
2022-04-11 15:33:39 +0100

    closes #2; Implement the methods that came with the code

--- projP3-09Mar/numbrix.py
+++ projP3-09Mar/numbrix.py
@@ -8,6 +8,7 @@
 
 import string
 import sys
+import this
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
 
 
@@ -28,49 +29,64 @@ class NumbrixState:
 class Board:
     """ Representacao interna de um tabuleiro de Numbrix. """
 
-    def __init__(self):
+    def __init__(self, size: int, initial_positions):
         """ O construtor especifica o estado inicial. """
-        self.size = None
-        self.positions = ()
+        self.size = size
+        self.positions = initial_positions
 
-    def get_number(self, row: int, col: int) -> int:
+    def get_number(self, row: int, col: int):
         """ Devolve o valor na respetiva posicao do tabuleiro. """
-        # TODO
-        pass
+        return self.positions[row][col]
     
-    def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
+    def adjacent_vertical_numbers(self, row: int, col: int):
         """ Devolve os valores imediatamente abaixo e acima, 
         respectivamente. """
-        # TODO
-        pass
+        if row == 0:
+            up = None
+        else:
+            up = self.get_number(row - 1, col)
+        if row == self.size - 1:
+            down = None
+        else:
+            down = self.get_number(row + 1, col)
+        return (up, down)
     
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """ Devolve os valores imediatamente a esquerda e a direita, 
         respectivamente. """
-        # TODO
-        pass
+        if col == 0:
+            left = None
+        else:
+            left = self.get_number(row, col - 1)
+        if col == self.size - 1:
+            right = None
+        else:
+            right = self.get_number(row, col + 1)
+        return (left, right)
     
     @staticmethod    
-    def parse_instance(self, filename: str):
+    def parse_instance(filename: str):
         """ Le o ficheiro cujo caminho e passado como argumento e retorna
         uma instancia da classe Board. """
+
         with open(filename, 'r') as file:
             data = file.read()
 
-        self.size = data[0]
-        i = 0
-        for l in range(self.size):
-            while i < len(data):
-                if data[i] != '\t':
-                    if i != '\n':
-                        l.append(i)
-                    else   
-                    
+        size = int(data[0])
 
+        initial_positions = ()
+        row = ()
+        for item in data[2::]:
+            if item != '\t' and item != '\n':
+                row += (int(item), )
+            if item == '\n':
+                initial_positions += (row, )
+                row = ()
+
+        return Board(size, initial_positions)
 
 
     # TODO: outros metodos da classe
-
 
 class Numbrix(Problem):
     def __init__(self, board: Board):
@@ -112,10 +128,9 @@ if __name__ == "__main__":
     # Ler o ficheiro de input de sys.argv[1],
     inputFile = sys.argv[1]
 
-    board = Board()
-    board.parse_instance(inputFile)
+    board = Board.parse_instance(inputFile)
+    print(board.adjacent_vertical_numbers(1, 2))
 
     # Usar uma tecnica de procura para resolver a instancia,
     # Retirar a solucao a partir do no resultante,
     # Imprimir para o standard output no formato indicado.
-    pass
